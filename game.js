7764a8b7286474557af698b90bdcae9f49047d18
kilkakat <kkilasoniya@gmail.com>
2024-04-24 19:26:34 +0300

    raw code

--- game.js
+++ game.js
@@ -1,18 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const grid = document.querySelector('.grid');
-    let width = 10;
-    let squares = [];
+const TILE_STATUSES = {
+    HIDDEN: "hidden",
+    MINE: "mine",
+    NUMBER: "number",
+    MARKED: "marked",
+}
 
-    function createBoard () {
-        for (i = 0; i < width * width; i++) {
-            const square = document.createElement('div');
-            square.setAttribute('id', i);
-            grid.appendChild(square);
-            squares.push(square);
-            
+export function createBoard(boardSize, numberOfMines) {
+
+    for (let i = 0; i < boardSize; i++) {
+        let row = [];
+        for (let j = 0; j < boardSize; j++) {
+            const element = document.createElement('div');
+            element.dataset.status =  TILE_STATUSES.HIDDEN;
+           
+            let tile = {
+                i,
+                j,
+                get status() {
+                    return this.element.dataset.status 
+                },
+                set status(value) {
+                    this.element.dataset.status = value;
+                }
+            }
+
+            row.push(tile);
         }
-    }
-    createBoard();
-})
 
+        board.push(row);
+    }
+    return board;
+}
 
---
+++ script.js
@@ -0,0 +1,15 @@
+import { createBoard } from "./game.js";
+
+const BOARD_SIZE = 10;
+const NUMBER_OF_MINES = 2;
+
+const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
+const boardElement = document.querySelector(".board");
+console.log(board);
+board.forEach(row => {
+    row.forEach(tile => {
+    boardElement.append(tile.element)
+    });
+});
+
+boardElement.style.setProperty('--size', BOARD_SIZE);
